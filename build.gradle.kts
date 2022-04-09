cda703e7cc628eafded45fcf7746380f339319ca
Matthias <12-build@web.de>
2022-04-09 22:46:39 +0200

    feat(tc): testing bumping

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ application {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("de.timoliacreative:tranclate:2.0.94-SNAPCHOT") {
+    implementation("de.timoliacreative:tranclate:2.0.99-SNAPCHOT") {
         isChanging = true
     }
 }
