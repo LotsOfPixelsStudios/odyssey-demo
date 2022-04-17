5777bee26b683e1b7f85b4e61c134c27f1786f1e
12rcu <56599453+12rcu@users.noreply.github.com>
2022-04-17 16:04:42 +0200

    apply shared data with "with"-operator

--- src/main/kotlin/com/tcreative/addons/soldier/SoldierShared.kt
+++ src/main/kotlin/com/tcreative/addons/soldier/SoldierShared.kt
@@ -13,35 +13,42 @@ import com.tcreative.devtools.tranclate.builder.spawnRules
 import com.tcreative.devtools.tranclate.systemaddon.entityapi.AddonEntity
 
 fun loadTextures(addonEntity: AddonEntity) {
-    addonEntity.resource {
-        textures(
-            arrayListOf(
-                getEntityTextureResource("soldier_npc_0.png"),
-                getEntityTextureResource("soldier_npc_1.png"),
-                getEntityTextureResource("soldier_npc_2.png"),
-                getEntityTextureResource("soldier_npc_3.png")
-            ), Query.variant
-        )
+    with(addonEntity) {
+        resource {
+            textures(
+                arrayListOf(
+                    getEntityTextureResource("soldier_npc_0.png"),
+                    getEntityTextureResource("soldier_npc_1.png"),
+                    getEntityTextureResource("soldier_npc_2.png"),
+                    getEntityTextureResource("soldier_npc_3.png")
+                ), Query.variant
+            )
+        }
     }
 }
 
 fun loadTextureCompGroups(behEntityComponentGroups: BehEntityComponentGroups) {
-    for (i in 0..3)
-        behEntityComponentGroups.componentGroup("texture_variant_$i") {
-            variant(i)
-        }
+    with(behEntityComponentGroups) {
+        for (i in 0..3)
+            componentGroup("texture_variant_$i") {
+                variant(i)
+            }
+    }
+
 }
 
 fun spawnEvent(behEntityEvents: BehEntityEvents) {
-    behEntityEvents.defaultBornEvent {
-        randomize {
-            for (i in 0..3)
-                randomComp {
-                    weight = 1
-                    add {
-                        componentGroup = "texture_variant_$i"
+    with(behEntityEvents) {
+        defaultBornEvent {
+            randomize {
+                for (i in 0..3)
+                    randomComp {
+                        weight = 1
+                        add {
+                            componentGroup = "texture_variant_$i"
+                        }
                     }
-                }
+            }
         }
     }
 }
@@ -70,68 +77,72 @@ fun soldierSpawnRules() {
 }
 
 fun sharedComponents(components: BehEntityComponents) {
-    components.collisionBox {
-        height = 1.8f
-        width = 0.6f
-    }
-    components.physics { }
-    components.pushable { }
-    components.navigationWalk {
-        avoidDamageBlocks = true
-        avoidWater = true
-        canPathOverWater = false
-    }
-    components.behEquipItem { }
-    components.movement {
-        value = 0.2f
-    }
-    components.jumpStatic { }
-    components.movementBasic { }
-    components.health {
-        value = 20
-    }
-    components.despawn {
-        despawnFromDistance()
-    }
-    components.scale {
-        value = 1.05f
-    }
-    components.behNearestAttackableTarget {
-        priority = 1
-        entityTypes {
-            type {
-                filters {
-                    isFamily(subject = Subject.OTHER, value = "monster")
+    with(components) {
+        collisionBox {
+            height = 1.8f
+            width = 0.6f
+        }
+        physics { }
+        pushable { }
+        navigationWalk {
+            avoidDamageBlocks = true
+            avoidWater = true
+            canPathOverWater = false
+        }
+        behEquipItem { }
+        movement {
+            value = 0.2f
+        }
+        jumpStatic { }
+        movementBasic { }
+        health {
+            value = 20
+        }
+        despawn {
+            despawnFromDistance()
+        }
+        scale {
+            value = 1.05f
+        }
+        behNearestAttackableTarget {
+            priority = 1
+            entityTypes {
+                type {
+                    filters {
+                        isFamily(subject = Subject.OTHER, value = "monster")
+                    }
                 }
             }
+            mustReach = false
+            mustSee = true
         }
-        mustReach = false
-        mustSee = true
-    }
-    components.behHurtByTarget {
-        priority = 0
-    }
-    components.behRandomStroll {
-        priority = 4
-    }
-    components.behLookAtPlayer {
-        priority = 5
-        lookDistance = 20f
-    }
-    components.behHurtByTarget {
-        priority = 0
-    }
-    components.behMoveToLand {
-        priority = 2
+        behHurtByTarget {
+            priority = 0
+        }
+        behRandomStroll {
+            priority = 4
+        }
+        behLookAtPlayer {
+            priority = 5
+            lookDistance = 20f
+        }
+        behHurtByTarget {
+            priority = 0
+        }
+        behMoveToLand {
+            priority = 2
+        }
     }
 }
 
 fun sharedResAnimControllers(animationControllers: AnimationControllers) {
-    animationControllers.animController("general") {
-        initialState = "default"
-        animStates {
-            animState("default") {
-                animation = arrayListOf("base_pose", "default", "move")
+    with(animationControllers) {
+        animController("general") {
+            initialState = "default"
+            animStates {
+                animState("default") {
+                    animation = arrayListOf("base_pose", "default", "move")
+                }
             }
         }
     }
